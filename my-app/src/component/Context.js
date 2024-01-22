ec2d0e16d47599a5a1a459e1aadaf9eda86da3c4
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2024-01-22 21:43:28 +0700

    21.43 22.01.2024

--- my-app/src/component/Context.js
+++ my-app/src/component/Context.js
@@ -2,96 +2,85 @@ import React, { useState } from 'react';
 import ButtonForXorO from './field/lineForFueld.js/ButtonForXorO';
 // import { v4 as uuidv4 } from 'uuid';
 
-
 export const Context = React.createContext({
-    cell: JSON.parse(localStorage.Cell),
-    setCell: ()=>{},
-    nextOorX: "X",
-    setNextOorX:()=>{},
-
+  cell: null,
+  setCell: () => {},
+  nextOorX: 'X',
+  setNextOorX: () => {},
 });
 
 export const ContextProvider = ({ children }) => {
-    
-    let valueCell = [
-        {
-            id: 1,
-            value: 1
-        },
-        {
-            id: 2,
-            value: 2
-        },
-        {
-            id: 3,
-            value: null
-        },
-        {
-            id: 4,
-            value: null
-        },
-        {
-            id: 5,
-            value: null
-        },
-        {
-            id: 6,
-            value: null
-        },
-        {
-            id: 7,
-            value: null
-        },
-        {
-            id: 8,
-            value: null
-        },
-        {
-            id: 9,
-            value: null
-        },
-    ]
-    if(localStorage?.Cell){
+  let valueCell = [
+    {
+      id: 1,
+      value: null,
+    },
+    {
+      id: 2,
+      value: null,
+    },
+    {
+      id: 3,
+      value: null,
+    },
+    {
+      id: 4,
+      value: null,
+    },
+    {
+      id: 5,
+      value: null,
+    },
+    {
+      id: 6,
+      value: null,
+    },
+    {
+      id: 7,
+      value: null,
+    },
+    {
+      id: 8,
+      value: null,
+    },
+    {
+      id: 9,
+      value: null,
+    },
+  ];
+  if (localStorage?.Cell) {
+  } else {
+    localStorage.Cell = JSON.stringify(valueCell);
+  }
+  if (localStorage?.NextOorX) {
+  } else {
+    localStorage.setItem('NextOorX', 'X');
+  }
 
-    } else{
-        localStorage.Cell = JSON.stringify(valueCell)
-    }
-    if(localStorage?.NextOorX){
+  const [cell, setCell] = useState(valueCell);
+  const [nextOorX, setNextOorX] = useState('X');
 
-    } else{
-        localStorage.setItem("NextOorX", "X")
-    }
-    
+  function tapOnCell(id) {
+    localStorage.Cell = JSON.stringify(
+      ButtonForXorO(id, cell, nextOorX, setNextOorX)
+    );
+    setCell(JSON.parse(localStorage.Cell));
+    // console.log(cell)
+  }
 
+  localStorage.setItem('NextOorX', nextOorX);
 
-    const [cell, setCell] = useState(valueCell)
-    const [nextOorX, setNextOorX] = useState("X")
-    
-
-
-
-
-
-function tapOnCell(id){
-    localStorage.Cell=JSON.stringify(ButtonForXorO(id, cell, nextOorX, setNextOorX))
-setCell(JSON.parse(localStorage.Cell))
-// console.log(cell)
-}
-
-localStorage.setItem("NextOorX", nextOorX)
-
-    return (
-        <Context.Provider
-          value={{
-            cell,
-            setCell,
-            nextOorX,
-            setNextOorX,
-            tapOnCell,
-
-          }}
-        >
-          {children}
-        </Context.Provider>
-      );
-    };
+  return (
+    <Context.Provider
+      value={{
+        cell,
+        setCell,
+        nextOorX,
+        setNextOorX,
+        tapOnCell,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
--- my-app/src/component/field/LineForField.js
+++ my-app/src/component/field/LineForField.js
@@ -1,16 +1,18 @@
-import { useContext } from "react"
-import { Context } from "../Context"
-import "./lineForField.css"
+import { useContext } from 'react';
+import { Context } from '../Context';
+import './lineForField.css';
 
-function LineForField(props){
-const {tapOnCell, cell} = useContext(Context)
+function LineForField(props) {
+  const { tapOnCell, cell } = useContext(Context);
 
-// console.log(cell)
-    function tap(id){
-        tapOnCell(id)
-    }
-return(
-<li className="call" id={props.id} onClick={(e)=>tap(e.target.id)}>{props.value}</li>
-)
+  // console.log(cell)
+  function tap(id) {
+    tapOnCell(id);
+  }
+  return (
+    <li className="call" id={props.id} onClick={(e) => tap(e.target.id)}>
+      <button>{props.value}</button>
+    </li>
+  );
 }
-export default LineForField
+export default LineForField;
