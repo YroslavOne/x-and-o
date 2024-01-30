7b76ab029c9387e785e4a83f322f81bf047f88b5
YroslavOne <berilloyaroslav@gmail.com>
2024-01-30 17:46:13 +0700

    17.46 30.01.2024

--- my-app/src/component/game/field/Score.js
+++ my-app/src/component/game/field/Score.js
@@ -1,14 +1,24 @@
 import { Context } from "../../Context"
 import { useContext } from "react"
+import "./score.css"
 
 function Score(){
     const {scoreList} = useContext(Context)
    
     return(
         <div className="block-score">
-            <h2 className="text-block-score">X - {scoreList.X}</h2> 
-            <h2 className="text-block-score">dead Heat - {scoreList.deadHeat}</h2> 
-            <h2 className="text-block-score">O - {scoreList.O}</h2> 
+            <div className="block-score-border thisx"> 
+                <h2 className="text-block-score">X (you)</h2>
+                <h1 className="texth1-block-score">{scoreList.X}</h1>
+                </div>
+                <div className="block-score-border this-dead-heat"> 
+                <h2 className="text-block-score">ties</h2>
+                <h1 className="texth1-block-score">{scoreList.deadHeat}</h1>
+                </div>
+                <div className="block-score-border thiso"> 
+                <h2 className="text-block-score">O (CPU)</h2>
+                <h1 className="texth1-block-score">{scoreList.O}</h1>
+                </div>
         </div>
     )
 }
